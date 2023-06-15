e01d4787c67b740f43e89ffe25d24ceb0da43985
a.karamiani <aziz.karamiani@gmail.com>
2023-06-15 12:45:14 +0330

    Initial Commit

---
+++ src/main/kotlin/com/tapture/resumablefileupload/ResumableFileUploadApplication.kt
@@ -0,0 +1,11 @@
+package com.tapture.resumablefileupload
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResumableFileUploadApplication
+
+fun main(args: Array<String>) {
+    runApplication<ResumableFileUploadApplication>(*args)
+}
---
+++ src/test/kotlin/com/tapture/resumablefileupload/ResumableFileUploadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tapture.resumablefileupload
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ResumableFileUploadApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
